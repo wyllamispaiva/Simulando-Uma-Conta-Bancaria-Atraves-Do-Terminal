28019f6147973a74e246f4e9518297e9cc2b12fc
wyllamis guimaraes paiva <101592222+wyllamispaiva@users.noreply.github.com>
2023-09-12 16:03:37 -0300

    Atualizando arquivos

---
+++ src/ExemploWhile.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+        int num = 5, count = 1;
+        do {
+            num += count;
+            System.out.println(num);
+        } while (count <= 3);
+    }
+}
---
+++ src/Exemplofo.java
@@ -0,0 +1,9 @@
+public class Exemplofo {
+   public static void main(String[] args) {
+     for (int carneirinhos = 1; carneirinhos<=20;carneirinhos ++ ) {
+        System.out.println("Contando carneirinhos "+carneirinhos);
+     }
+             System.out.println("Joãozinho dormiu ");
+  
+    }
+}
